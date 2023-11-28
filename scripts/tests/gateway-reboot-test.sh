0128d581965eb3e901c9c64a698ea7e2b3866904
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2023-11-27 23:11:31 -0500

    Removed old nix

--- scripts/tests/gateway-reboot-test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Runs a test to make sure gateways can reboot properly, with the expected state
-
-set -euo pipefail
-export RUST_LOG="${RUST_LOG:-info}"
-source ./scripts/build.sh
-
-devimint gateway-reboot-test
