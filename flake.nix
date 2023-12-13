9ab9967e53208ff67e7627baa9b10f6cd276779b
Roman Volosatovs <rvolosatovs@riseup.net>
2023-12-13 15:00:52 +0100

    build(nix): nixify the project
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,13 +1,23 @@
 {
-  inputs.nixify.url = github:rvolosatovs/nixify;
+  inputs.nixify.url = "github:rvolosatovs/nixify";
 
   outputs = {nixify, ...}:
     nixify.lib.rust.mkFlake {
       src = ./.;
-      name = "rust-workspace";
+      name = "RustStore";
 
+      # NOTE: `IronAge` is outside this workspace and will not be tested/linted,
+      # perhaps that should be changed
       build.workspace = true;
       clippy.workspace = true;
       test.workspace = true;
+
+      buildOverrides = {pkgs, ...}: {buildInputs ? [], ...}:
+        with pkgs.lib; {
+          buildInputs =
+            buildInputs
+            ++ optional pkgs.stdenv.hostPlatform.isDarwin pkgs.libiconv;
+        };
     };
 }
+
